d651ff9d55f65c352ef0d1f49d2e84e75915cef8
nocodeci <yohankoffik@gmail.com>
2025-02-10 15:42:19 +0000

    full version

--- pressing/src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyD0dRPv9NgTyFqAzuKHDI6yuYtBZ0ebTkY",
-    authDomain: "pressing-4d9d5.firebaseapp.com",
-    projectId: "pressing-4d9d5",
-    storageBucket: "pressing-4d9d5.firebasestorage.app",
-    messagingSenderId: "796754556231",
-    appId: "1:796754556231:web:117dc582fb9ef54ac2c862",
-    measurementId: "G-C4BEGE5QM6"
-  };
